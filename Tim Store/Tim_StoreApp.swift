04639d1fe67442bcf12437f27ee31c33a4c75e6b
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2024-07-05 00:32:45 +0100

    Initial Commit

---
+++ Tim Store/Tim_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tim_StoreApp.swift
+//  Tim Store
+//
+//  Created by Godwin IE on 05/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Tim_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
